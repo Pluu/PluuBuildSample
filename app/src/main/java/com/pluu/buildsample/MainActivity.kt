ed45efacdf293aab94aa86d4f660bc73cf8d0e69
pluu <pluulove@gmail.com>
2024-12-24 23:57:08 +0900

    미동작하는 케이스 제거

--- app/src/main/java/com/pluu/buildsample/MainActivity.kt
+++ app/src/main/java/com/pluu/buildsample/MainActivity.kt
@@ -2,7 +2,6 @@ package com.pluu.buildsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.pluu.ConstStaticBuildConfiguration
 import com.pluu.FlavorBuildConfiguration
 import com.pluu.StaticBuildConfiguration
 import com.pluu.anylibrary.AnySampleActivity
@@ -18,9 +17,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.tvStaticTextView.text = StaticBuildConfiguration.path
-
-        // Const로 정의한 경우는 모듈마다 동이하지 값이 동일하지 않음
-        binding.tvStaticConstTextView.text = ConstStaticBuildConfiguration.path
 
         binding.tvFlavorTextView.text = FlavorBuildConfiguration().path()
 
