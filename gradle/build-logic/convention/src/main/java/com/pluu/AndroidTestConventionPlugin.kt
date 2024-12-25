df43abd5d3f384a734cd5104dedefff1d8e50f95
pluu <pluulove@gmail.com>
2024-12-25 17:22:51 +0900

    debug 시에만 testing 폴더 연결
    - disableUnnecessaryAndroidTests

--- gradle/build-logic/convention/src/main/java/com/pluu/AndroidTestConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/java/com/pluu/AndroidTestConventionPlugin.kt
@@ -1,9 +1,13 @@
 package com.pluu
 
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.variant.LibraryAndroidComponentsExtension
+import com.pluu.convention.utils.disableUnnecessaryAndroidTests
 import com.pluu.convention.utils.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
+import java.io.File
 
 @Suppress("unused")
 class AndroidTestConventionPlugin : Plugin<Project> {
@@ -11,6 +15,32 @@ class AndroidTestConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("org.jetbrains.kotlin.android")
+            }
+
+            extensions.getByType(CommonExtension::class.java).apply {
+                sourceSets {
+                    getByName("debug") {
+                        assets.srcDirs(
+                            File(rootDir, "testing/assets")
+                        )
+                    }
+                }
+            }
+
+//            extensions.findByType(AndroidComponentsExtension::class.java)?.apply {
+//                onVariants {
+//                    if (it.name.endsWith("debug", ignoreCase = true)) {
+//                        it.sources.assets?.addStaticSourceDirectory(
+//                            File(rootDir, "testing/assets").path
+//                        )
+//                    }
+//                }
+//            }
+
+            extensions.findByType(LibraryAndroidComponentsExtension::class.java)?.let {
+                with(it) {
+                    disableUnnecessaryAndroidTests(target)
+                }
             }
 
             dependencies {
